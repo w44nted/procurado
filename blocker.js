1a46bb565ec7fb7f1c3487d0568d45f06c52a636
victor <147956116+w44nted@users.noreply.github.com>
2024-01-13 15:46:34 -0300

    Update blocker.js

--- blocker.js
+++ blocker.js
@@ -2,25 +2,41 @@ mode = 1;
 document.oncontextmenu = new Function("return false;")
 
 function getKey(e) {
-    var key = e.keyCode;
-    console.log(key);
+  var key = e.keyCode;
+  console.log(key);
 
-    if (key == 16 || key == 17) {
-        mode = 2;
-    }
-                
-    if (mode == 1) {
-        if (key == 123) {
-           return false;
-        }
-    } else {
-        if (key == 73 || key == 74 || key == 85) {
-            return false;
-        } else {
-            if (key == 123) {
-                return false;
-                }
-            }
-        }
+  if (key == 16 || key == 17) {
+    mode = 2;
+  }
+
+  if (key == 17) {
+    return false;
+  }
+
+  if (mode == 1) {
+    if (key == 123) {
+      return false;
+    }
+
+    if (key == 65 && mode == 1) {
+      return false;
+    }
+
+    if (key == 67 && mode == 1) {
+      return false;
+    }
+
+    if (key == 86 && mode == 1) {
+      return false;
+    }
+  } else {
+    if (key == 73 || key == 74 || key == 85) {
+      return false;
+    } else {
+      if (key == 123) {
+        return false;
+        }
+      }
+    }
 }
 window.onkeydown = getKey;
